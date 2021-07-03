48d3b69819af0f0798691897d6c393f480a84c0b
Piotr Nikonowicz <piotr.nikonowicz@gmail.com>
2021-07-03 17:20:31 +0200

    Added vue to the project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main-wrapper">
+        <h1>
+            I am an example vue component!
+        </h1>
+        <div>
+            <p>
+                Input sample value here:
+                <input type="text" v-model="sampleValue">
+            </p>
+        </div>
+        <div class="">
+            <p>
+                Sample value is: <span v-text="sampleValue" style="font-weight: bold;"></span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            sampleValue: 0,
+        }
+    },
+    mounted() {
+        console.log('Example component is mounted!');
+    }
+}
+</script>
